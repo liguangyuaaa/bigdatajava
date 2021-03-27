b559a0bf8c0182351563a0f7586ac0972fd1ad38
liguangyu03 <liguangyu03@58.com>
2021-03-27 15:25:29 +0800

    1111

--- src/main/java/lgystudy/day01.java
+++ src/main/java/lgystudy/day01.java
@@ -3,7 +3,7 @@ package lgystudy;
 public class day01 {
     public static void main(String[] args) {
         System.out.println("hello word");
-        System.out.println("java1");
+
         System.out.println("java2");
         System.out.println("java3");
 
