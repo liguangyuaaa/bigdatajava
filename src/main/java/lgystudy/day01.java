b32200281576825aace96544fe09ad3444227f20
liguangyu03 <liguangyu03@58.com>
2021-03-27 11:44:10 +0800

    1111

--- src/main/java/lgystudy/day01.java
+++ src/main/java/lgystudy/day01.java
@@ -4,5 +4,8 @@ public class day01 {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("java1");
+        System.out.println("java2");
+
+
     }
 }
