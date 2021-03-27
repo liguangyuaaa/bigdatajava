f7f883f4f3f37b47f154f5028aa69f7917c26c2f
liguangyuaaa <guangyu95@126.com>
2021-03-27 19:25:54 +0800

    1122

--- src/main/java/lgystudy/day01.java
+++ src/main/java/lgystudy/day01.java
@@ -4,6 +4,7 @@ public class day01 {
     public static void main(String[] args) {
         System.out.println("hello word");
 
+        System.out.println("sdfas");
         System.out.println("java2");
         System.out.println("java3");
         System.out.println("java4");
