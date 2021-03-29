c268b088155409cc6dfb9a4c47ebf5cb392af6ba
liguangyu03 <liguangyu03@58.com>
2021-03-29 10:35:00 +0800

    lgy2222222222222

--- src/main/java/lgystudy/day2_06_20210327/src7/day01.java
+++ src/main/java/lgystudy/day2_06_20210327/src7/day01.java
@@ -8,6 +8,8 @@ public class day01 {
         UserLgy userLgy = new UserLgy("tom",10);
         System.out.println(userLgy.toString());
         System.out.println("java11111111111");
+        System.out.println("java222222");
+
 
 
     }
