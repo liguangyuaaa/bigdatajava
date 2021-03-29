c76973a25c4676a23272f2d275a3f8e69f07fe57
Shame <tzhigang@hotmail.com>
2021-03-29 11:14:20 +0800

    tzg  day02任务上传

---
+++ src/main/java/tzgstudy/day02/exer/ListExer.java
@@ -0,0 +1,43 @@
+package tzgstudy.day02.exer;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExer {
+
+    /*
+    区分List中remove（int index） 和remove（Object obj）
+     */
+    @Test
+    public void testListRemove(){
+
+        List list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        updateObjectList(list);
+        System.out.println("list = " + list);
+
+    }
+
+    /**
+     * remove的是索引
+     * @param list
+     */
+    private void updateIndexList(List list){
+        list.remove(2);
+    }
+
+    /**
+     * remove的在集合中等于该值的数值 当有重复数据时移除第一个
+     * @param list
+     */
+    private void updateObjectList(List list){
+        list.remove(new Integer(2));
+    }
+
+}
