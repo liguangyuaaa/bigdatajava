9901990c9d66adc780abc0fc60a77a139785f8c3
liguangyuaaa <guangyu95@126.com>
2021-03-31 23:47:16 +0800

    上传20210329到20210331日常任务

---
+++ src/main/java/day2_08_202103_29_30_31/src/com/atguigu/java1/CustomerDAO.java
@@ -0,0 +1,8 @@
+package day2_08_202103_29_30_31.src.com.atguigu.java1;
+
+/**
+ * @author shkstart
+ * @create 2019 上午 11:55
+ */
+public class CustomerDAO extends DAO<Customer>{//只能操作某一个表的DAO
+}
---
+++ src/main/java/day2_08_202103_29_30_31/src/com/atguigu/java2/Person.java
@@ -0,0 +1,8 @@
+package day2_08_202103_29_30_31.src.com.atguigu.java2;
+
+/**
+ * @author shkstart
+ * @create 2019 下午 2:42
+ */
+public class Person {
+}
