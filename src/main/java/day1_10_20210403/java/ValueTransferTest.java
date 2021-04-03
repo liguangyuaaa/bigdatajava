9801ca03237006a56d67d3fa177727b231d2835e
liguangyu03 <liguangyu03@58.com>
2021-04-03 21:29:59 +0800

    上传0403任务

---
+++ src/main/java/day1_10_20210403/java/ValueTransferTest.java
@@ -0,0 +1,19 @@
+package day1_10_20210403.java;
+
+public class ValueTransferTest {
+	public static void main(String[] args) {
+		
+		String s1 = "hello";
+		
+		ValueTransferTest test = new ValueTransferTest();
+		test.change(s1);
+		
+		System.out.println(s1);//hi~~
+		
+		
+	}
+	
+	public void change(String s){
+		s = "hi~~";
+	}
+}
---
+++ src/main/java/day1_10_20210403/java2/java3/Dog.java
@@ -0,0 +1,5 @@
+package day1_10_20210403.java2.java3;
+
+public class Dog {
+
+}
