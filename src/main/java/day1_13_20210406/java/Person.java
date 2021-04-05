3d95979be9dd99528dbd7f39c60f72da61351e3b
liguangyu03 <liguangyu03@58.com>
2021-04-05 18:06:57 +0800

    上传0406日常任务

---
+++ src/main/java/day1_13_20210406/java/Person.java
@@ -0,0 +1,17 @@
+package day1_13_20210406.java;
+
+public class Person {
+	String name;
+	int age;
+	
+	int id = 1001;
+	
+	public void eat(){
+		System.out.println("人：吃饭");
+	}
+	
+	public void walk(){
+		System.out.println("人：走路");
+	}
+	
+}
