607dafb501e26c890c95ebddcef108c113a47a5b
liguangyu <guangyu05@126.com>
2021-05-10 00:15:30 +0800

    20210510flink付款超时

---
+++ src/main/java/lgystudy/flink/flink_20210509/TxPayMatchByJoin.java
@@ -0,0 +1,81 @@
+package lgystudy.flink.flink_20210509;
+
+import lgystudy.flink.flink_20210506.OrderEvent;
+import lgystudy.flink.flink_20210508.ReceiptEvent;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.ProcessJoinFunction;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+
+import java.net.URL;
+
+public class TxPayMatchByJoin {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.setParallelism(1);
+
+        //读取数据并转换成POJO类型
+        //读取订单支付事件数据
+        /*
+    private Long orderId;
+    private String eventType;
+    private String txId;
+    private Long timestamp;
+         */
+        URL orderResource = TxPayMatchByJoin.class.getResource("/OrderLog.csv");
+        DataStream<OrderEvent> orderEventDataStream = env.readTextFile(orderResource.getPath())
+                .map(line -> {
+                    String[] fields = line.split(",");
+                    return new OrderEvent(new Long(fields[0]), fields[1], fields[2], new Long(fields[3]));
+                })
+                .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<OrderEvent>() {
+                    @Override
+                    public long extractAscendingTimestamp(OrderEvent element) {
+                        return element.getTimestamp() * 1000L;
+                    }
+                })
+                .filter(data -> !"".equals(data.getTxId()));// 交易id不为空，必须是pay事件
+/**
+ *     private String txId;
+ *     private String payChannel;
+ *     private Long timestamp;
+ */
+        //读取到账事件数据
+        URL receiptResource = TxPayMatchByJoin.class.getResource("/ReceiptLong.csv");
+        SingleOutputStreamOperator<ReceiptEvent> receiptEventStream = env.readTextFile(receiptResource.getPath())
+                .map(line -> {
+                    String[] fields = line.split(",");
+                    return new ReceiptEvent(fields[0], fields[1], new Long(fields[2]));
+                })
+                .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<ReceiptEvent>() {
+                    @Override
+                    public long extractAscendingTimestamp(ReceiptEvent element) {
+                        return element.getTimestamp() * 1000L;
+                    }
+                });
+        //区间连接两条流，得到匹配的数据
+        SingleOutputStreamOperator<Tuple2<OrderEvent, ReceiptEvent>> resultStream = orderEventDataStream
+                .keyBy(OrderEvent::getTxId)
+                .intervalJoin(receiptEventStream.keyBy(ReceiptEvent::getTxId))
+                .between(Time.seconds(-3), Time.seconds(5))
+                .process(new TxPayMatchDetectByJoin());
+
+        resultStream.print();
+        env.execute("tx pay match by join job");
+    }
+
+    // 实现自定义ProcessJoinFunction
+    public static class TxPayMatchDetectByJoin extends ProcessJoinFunction<OrderEvent, ReceiptEvent, Tuple2<OrderEvent, ReceiptEvent>> {
+        @Override
+        public void processElement(OrderEvent left, ReceiptEvent right, Context ctx, Collector<Tuple2<OrderEvent, ReceiptEvent>> out) throws Exception {
+            out.collect(new Tuple2<>(left, right));
+        }
+    }
+}
