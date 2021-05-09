b9b98876e386fcb7ccca727c9988ff05467d57fb
liguangyu <guangyu05@126.com>
2021-05-09 20:48:41 +0800

    20210509flink付款超时

---
+++ src/main/java/lgystudy/flink/flink_20210508/OrderPayTimeout.java
@@ -0,0 +1,31 @@
+package lgystudy.flink.flink_20210508;
+import lgystudy.flink.flink_20210506.OrderEvent;
+import org.apache.flink.cep.CEP;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+
+import java.net.URL;
+
+public class OrderPayTimeout {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.setParallelism(1);
+
+        //读取数据并转换成pojo类型
+        URL resource = OrderPayTimeout.class.getResource("/OrderLog.csv");
+        env.readTextFile(resource.getPath())
+                .map(line -> {
+                    String[] fields = line.split(",");
+                    return new OrderEvent(new Long(fields[0]), fields[1], fields[2], new Long(fields[3]));
+                })
+                .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<OrderEvent>() {
+                    @Override
+                    public long extractAscendingTimestamp(OrderEvent element) {
+                        return element.getTimestamp() * 1000L;
+                    }
+                });
+        //1
+    }
+}
